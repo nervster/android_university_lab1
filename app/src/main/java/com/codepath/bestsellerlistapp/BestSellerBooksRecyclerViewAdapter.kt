0721ac13ba5a7ecdbf1096457163ad71ad914886
nervster <npsheth93@live.com>
2022-02-21 10:23:17 -0600

    "Update book layout with ranking, image, description and buy button"

--- app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBooksRecyclerViewAdapter.kt
+++ app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBooksRecyclerViewAdapter.kt
@@ -1,16 +1,21 @@
 package com.codepath.bestsellerlistapp
 
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.bumptech.glide.Glide
 import com.codepath.bestsellerlistapp.BestSellerBooksRecyclerViewAdapter.BookViewHolder
 import com.codepath.bestsellerlistapp.R.id
 import com.codepath.bestsellerlistapp.R.layout
 import com.codepath.bestsellerlistapp.models.BestSellerBook
+
 
 /**
  * [RecyclerView.Adapter] that can display a [BestSellerBook] and makes a call to the
@@ -28,8 +33,16 @@ class BestSellerBooksRecyclerViewAdapter(
 
     override fun onBindViewHolder(holder: BookViewHolder, position: Int) {
         holder.mItem = books[position]
+        holder.mBookRank.text = String.format("%d", books[position].rank)
         holder.mBookTitle.text = books[position].title
         holder.mBookAuthor.text = books[position].author
+        holder.mBookDescription.text = books[position].description
+//        holder.mBookBuyLink.setOnClickListener = books[position].amazonUrl;
+        val bestSellerBook = books[position]
+        Glide.with(holder.mView)
+            .load(bestSellerBook.bookImageUrl)
+            .centerInside()
+            .into(holder.mBookImage);
         holder.mView.setOnClickListener {
             holder.mItem?.let { book ->
                 mListener?.onItemClick(book)
@@ -42,8 +55,13 @@ class BestSellerBooksRecyclerViewAdapter(
     }
 
     inner class BookViewHolder(val mView: View) : ViewHolder(mView) {
+
         val mBookTitle: TextView = mView.findViewById<View>(id.book_title) as TextView
         val mBookAuthor: TextView = mView.findViewById<View>(id.book_author) as TextView
+        val mBookRank: TextView = mView.findViewById<View>(id.ranking) as TextView
+        val mBookDescription: TextView = mView.findViewById<View>(id.book_description) as TextView
+        val mBookImage: ImageView = mView.findViewById<ImageView>(id.book_image) as ImageView
+//        val mBookBuyLink: Button = mView.findViewById<Button>(id.buy_button) as Button
         var mItem: BestSellerBook? = null
 
         override fun toString(): String {
